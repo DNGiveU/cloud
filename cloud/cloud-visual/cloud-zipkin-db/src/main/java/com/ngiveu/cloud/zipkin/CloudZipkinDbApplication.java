cf7118047f69691172e04715f3fb3685f0f347e7
gaz <443175998@qq.com>
2018-04-12 21:56:46 +0800

    up zikpin 链路跟踪修正

--- cloud/cloud-visual/cloud-zipkin-db/src/main/java/com/ngiveu/cloud/zipkin/CloudZipkinDbApplication.java
+++ cloud/cloud-visual/cloud-zipkin-db/src/main/java/com/ngiveu/cloud/zipkin/CloudZipkinDbApplication.java
@@ -1,4 +1,4 @@
-package com.github.pig.zipkin;
+package com.ngiveu.cloud.zipkin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,15 +6,15 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import zipkin.server.EnableZipkinServer;
 
 /**
- * @author lengleng
+ * @author gaz
  * @date 2018-01-24
  * zipkin mysql 存储实现
  */
 @EnableDiscoveryClient
 @SpringBootApplication
 @EnableZipkinServer
-public class PigZipkinDbApplication {
+public class CloudZipkinDbApplication {
     public static void main(String[] args) {
-        SpringApplication.run(PigZipkinDbApplication.class, args);
+        SpringApplication.run(CloudZipkinDbApplication.class, args);
     }
 }
